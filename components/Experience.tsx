aa54d4013d837cc54994be8abc68595d412ba4ec
CodeOlga <111949555+CodeOlga@users.noreply.github.com>
2024-08-11 19:15:38 +0300

    bg experience card change

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -41,3 +41,6 @@ const Experience = () => {
 }
 
 export default Experience
+
+
+
--- components/ui/MovingBorders.tsx
+++ components/ui/MovingBorders.tsx
@@ -47,7 +47,7 @@ export function Button({
         <MovingBorder duration={duration} rx="30%" ry="30%">
           <div
             className={cn(
-              "h-20 w-20 opacity-[0.8] bg-[radial-gradient(var(--sky-500)_40%,transparent_60%)]",
+              "h-20 w-20 opacity-[0.8] bg-[radial-gradient(#CBACF9_40%,transparent_60%)]",
               borderClassName
             )}
           />
@@ -56,7 +56,7 @@ export function Button({
 
       <div
         className={cn(
-          "relative bg-slate-900/[0.8] border border-slate-800 backdrop-blur-xl text-white flex items-center justify-center w-full h-full text-sm antialiased",
+          "relative bg-slate-900/[0.2] border border-slate-800 backdrop-blur-xl text-white flex items-center justify-center w-full h-full text-sm antialiased",
           className
         )}
         style={{
