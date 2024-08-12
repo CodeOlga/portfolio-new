12e777f36d7d8e03a590f6bb4e53471c3e335410
CodeOlga <111949555+CodeOlga@users.noreply.github.com>
2024-08-12 10:41:56 +0300

    add social links

--- data/index.ts
+++ data/index.ts
@@ -205,13 +205,16 @@ export const socialMedia = [
   {
     id: 1,
     img: "/git.svg",
+    link: "https://github.com/CodeOlga",
   },
   {
     id: 2,
     img: "/twit.svg",
+    link: "https://github.com/CodeOlga",
   },
   {
     id: 3,
     img: "/link.svg",
+    link: "https://www.linkedin.com/in/olga-kryvutsenko/",
   },
 ];
